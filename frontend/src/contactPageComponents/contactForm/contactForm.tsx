30f6137b7f88856590efbdf10daa4280e079e02d
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-06-04 00:56:46 -0700

    Updated screen responsiveness and css styling for contact form

--- frontend/src/contactPageComponents/contactForm/contactForm.tsx
+++ frontend/src/contactPageComponents/contactForm/contactForm.tsx
@@ -4,30 +4,39 @@ import { useForm, ValidationError } from "@formspree/react";
 export default function ContactForm() {
   const [state, handleSubmit] = useForm("myzjyroa");
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+      return <p>Thanks for joining!</p>;
   }
-
+  
   return (
+
     <form onSubmit={handleSubmit} className="form-container">
+      <h2 className="form-header">Contact Me</h2>
       <input
         id="email"
-        type="email"
+        type="email" 
         name="email"
         className="email-field"
         placeholder="Email"
       />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
       <textarea
         id="message"
         name="message"
         className="content-field"
         placeholder="Message"
       />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button
-        type="submit"
-        disabled={state.submitting}
-        className="submit-button"
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" 
+      disabled={state.submitting}
+      className="submit-button"
       >
         Submit
       </button>
